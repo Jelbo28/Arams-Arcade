a85bdfc55e4d4f6afaac68223cb0e5c2bb9ff1f3
Aram Joseph <ajoseph@utc4me.org>
2017-05-21 11:56:26 -0400

    Cleaning up files

---
+++ Assets/Textures/Scripts/Minigame Results/MinigameCanvas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MinigameCanvas : MonoBehaviour
+{
+    private Text titleText;
+    private Text minigameInfoText;
+    private SceneChanger sceneChanger;
+    void Start ()
+	{
+	    titleText = GetComponent<Text>();
+        minigameInfoText = transform.GetChild(1).GetComponent<Text>();
+        sceneChanger = FindObjectOfType<SceneChanger>();
+        titleText.text = sceneChanger.currentMinigame.minigameName;
+        minigameInfoText.text = sceneChanger.currentMinigame.gameInfo;
+    }
+}
