97cd3bc9cab500f25fd8ad71077bb00b5af323bf
Aram Joseph <ajoseph@utc4me.org>
2019-12-27 20:55:36 -0500

    Fixed some graphical issues and added moving space animation

---
+++ Assets/Scripts/_General/Level Main/SpaceMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceMovement : MonoBehaviour
+{
+    [SerializeField]
+    float rotationSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed);
+    }
+}
